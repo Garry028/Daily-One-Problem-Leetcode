efd1e88544eb4b0e54e1f4ea68e70399a8971940
Garry <gmisal2002@gmail.com>
2022-07-15 15:30:55 +0530

    done with

---
+++ Final_value_of_Variable_after_operations/fvovao.java
@@ -0,0 +1,19 @@
+// class Solution {
+//     public int finalValueAfterOperations(String[] operations) {
+//         int x=0;
+//         for(int i=0;i<operations.length;i++)
+//         {
+//             if(operations[i].charAt(1)=='+') // if the string contains '+' at 1st index then increament number 
+//             {
+//                 x++;
+//             }
+//             else
+//             {
+//                 x--;
+//             }
+//         }
+//         return x;
+//     }
+// }
+
+// TC -> O(n) & SC -> O(1)

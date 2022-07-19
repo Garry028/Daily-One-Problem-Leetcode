fa97a3065ef8c214d4dfb25a9f59ca14137dffff
Garry <gmisal2002@gmail.com>
2022-07-19 17:59:02 +0530

    done

---
+++ Ceiling_Problem/Ceiling.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        
+        while(start<=end)
+        {
+            int mid= start + (end-start)/2;
+            
+            if(target>nums[mid])
+            {
+                start=mid+1;
+            }
+            else if(target<nums[mid])
+            {
+                end=mid-1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
+
+// when element not found then the while condition breaks.
+// then senario is like ->  end - mid - start   means if we return start then we will get the element greater than target.
